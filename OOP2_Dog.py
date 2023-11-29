4cec05213b415428c68da56dc083e6cf0664a049
ism-courses <152141243+ism-courses@users.noreply.github.com>
2023-11-29 11:14:42 -0700

    Add files via upload

---
+++ OOP2_Dog.py
@@ -0,0 +1,6 @@
+class Dog:
+    def __init__(self, age, name, is_male, weight):
+        self.age = age
+        self.name = name
+        self.is_male = is_male # Boolean. True if Male, False if Female.
+        self.weight = weight
